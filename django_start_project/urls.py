cc33d027d31b06b55e8574a2589fdea5acd7ea6b
Eduardo Denis <edigde@gmail.com>
2020-02-13 11:35:09 -0300

    Django login basico

---
+++ django_start_project/urls.py
@@ -0,0 +1,13 @@
+"""Platzigram URLs module."""
+
+# Django
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path, include
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('users/', include(('users.urls', 'users'), namespace='users')),
+]
